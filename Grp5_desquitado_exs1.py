27c97aa299208eba85e636246b9e777f63cee80f
Irene031600 <desquitadoirene_bscs@plmun.edu.ph>
2024-09-11 14:27:42 +0800

    Create Grp5_desquitado_exs1.py

---
+++ Grp5_desquitado_exs1.py
@@ -0,0 +1,27 @@
+#My first simple script
+
+print ("Hello,I,m Irene Desquitado! BSCS 3F") age = 24 name = "Irene"
+
+print("Age: ",age) print("Name: ",name)
+
+Elopre=" Elopre"
+angela="angela"
+judith= "judith"
+jane=" jane"
+
+age, name, gender = 22, "Irene", "female" print ("Age:",age) print ("Name:", name)
+
+print ("Gender:",gender)
+
+age, edad, num = 24,24, 24 print(age, edad, num)- 👋 Hi, I’m @Irene031600
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ...
+
+<!---
+Irene031600/Irene031600 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
